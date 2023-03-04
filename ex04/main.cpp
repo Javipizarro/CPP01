369b581f3ca4977c4d782e3d9bb6507fd0457c3e
Javipizarro <javipizarro@gmail.com>
2023-03-04 22:16:17 +0100

    ex04 done, no classes needed

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jpizarro <jpizarro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:02:30 by jpizarro          #+#    #+#             */
-/*   Updated: 2023/03/01 12:20:47 by jpizarro         ###   ########.fr       */
+/*   Updated: 2023/03/04 22:13:58 by jpizarro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,59 @@
 
 using namespace std;
 
-int		main(int argc, char *argv[]) {
-	std::ifstream iFile;
-	std::ofstream oFile;
-
+bool	invalidArgs(int argc) {
 	if (argc > 4) {
 		cout << "Too many arguments" << endl;
-		return (1);
-//	} else if (argv < 4) {
-//		cout << "Too few arguments" << endl;
-		return (1);
+		return true;
+	} else if (argc < 4) {
+		cout << "Too few arguments" << endl;
+		return true;
 	}
+	return false;
+}
 
-
-	iFile.open(argv[1]);
+bool	openFiles(ifstream& iFile, ofstream& oFile, string filePath) {
+	iFile.open(filePath);
 	if (!iFile.is_open()) {
-		cout << "Error while opening input file " << argv[1] << endl;
-		return (1);
+		cout << "Error while opening input file " << filePath << endl;
+		return false;
 	}
 
-	string oFileName = argv[1];
-	oFileName.append(".replace");
-	oFile.open(oFileName);
+	filePath.append(".replace");
+	oFile.open(filePath);
 	if (!oFile.is_open()) {
-		cout << "Error while creating output file " << oFileName << endl;
+		cout << "Error while creating output file " << filePath << endl;
 		iFile.close();
-		return (1);
+		return false;
 	}
-	
-//	string line;
-//	for (int i = 0; getline(iFile, line); i++) {
-//		i ? oFile << endl << line : oFile << line;
-//	}
+	return true;
+}
 
-	string line, buff;
+void	replace(ifstream& iFile, ofstream& oFile, const string s1, const string s2) {
+	string	line, buff;
+	int		eraseLen = s1.length(), lastPos = 0;
+	
 	for (int i = 0; getline(iFile, line); i++) {
 		if (i)
 			buff.append("\n");
 		buff.append(line);
 	}
+	while ((lastPos = buff.find(s1, lastPos)) != -1) {
+		buff.erase(lastPos, eraseLen);
+		buff.insert(lastPos, s2);
+	}
 	oFile << buff;
+}
 
+int		main(int argc, char *argv[]) {
+	ifstream iFile;
+	ofstream oFile;
+
+	if (invalidArgs(argc))
+		return (1);
+	if (!openFiles(iFile, oFile, argv[1]))
+		return (2);
+	replace(iFile, oFile, argv[2], argv[3]);
 	iFile.close();
 	oFile.close();
 
